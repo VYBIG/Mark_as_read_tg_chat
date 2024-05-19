bda6498bcf2f204ca756845cc1bd0d412277c51a
VY_BIG <vybigvall@gmail.com>
2024-05-19 15:11:19 +0300

    remove_comments

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from telethon.sync import TelegramClient
 from telethon import events, types
 
-# These example values won't work. You must get your own api_id and
-# api_hash from https://my.telegram.org, under API Development.
+
 api_id = 27326624
 api_hash = '261863acefacdc9d7ab5426cecd4631b'
 
@@ -19,21 +18,14 @@ print('Start to read message from Archive')
 async def mark_archived_dialogs_as_read(event):
     async for dialog in client.iter_dialogs(archived=True):
         if event.message.id == dialog.message.id:
-            # print(f'Чат {dialog.name} в Архиве')
             if isinstance(event.peer_id, types.PeerChat):
-                # print(f'Чат {dialog.name} это Группа')
                 await client.send_read_acknowledge(entity=event.message.peer_id.chat_id,
-                                                   # message=event.message.message,
-                                                   # max_id = 0,
                                                    max_id=event.message.id,
                                                    clear_mentions=True,
                                                    clear_reactions=True,
                                                    )
             elif isinstance(event.peer_id, types.PeerChannel):
-                # print(f'Чат {dialog.name} это Канал')
                 await client.send_read_acknowledge(entity=event.message.peer_id.channel_id,
-                                                   # message=event.message.message,
-                                                   # max_id=0,
                                                    max_id=event.message.id,
                                                    clear_mentions=True,
                                                    clear_reactions=True,
