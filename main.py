5c419c589b3bca54067554cee927308f85b85d4c
VY_BIG <vybigvall@gmail.com>
2024-05-25 22:07:45 +0300

    remove_unuseless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon import events, types
-
+from telethon.tl.functions.messages import ReadHistoryRequest
 
 api_id = 27326624
 api_hash = '261863acefacdc9d7ab5426cecd4631b'
@@ -11,6 +11,19 @@ client = TelegramClient('/root/read_messages/read_archived_message/VY_BIG_Client
 client.start(phone='+79619062680')
 
 print('Start to read message from Archive')
+
+
+
+@client.on(events.NewMessage(incoming=True))
+async def mark_archived_dialogs_as_read(event):
+    async for dialog in client.iter_dialogs(archived=True):
+        if event.message.message == dialog.message.message:
+            await client.send_read_acknowledge(entity=await client.get_entity(dialog.name),
+                                               max_id=event.message.id,
+                                               clear_mentions=True,
+                                               clear_reactions=True,
+                                               )
+
 
 
 @client.on(events.ChatAction())
@@ -29,21 +42,7 @@ async def mark_chat_action_as_read(event):
                                            )
 
 
-@client.on(events.NewMessage(incoming=True))
-async def mark_archived_dialogs_as_read(event):
-    async for dialog in client.iter_dialogs(archived=True):
-        if event.message.message == dialog.message.message:
-            if isinstance(event.peer_id, types.PeerChat):
-                await client.send_read_acknowledge(entity=await client.get_entity(dialog.name),
-                                                   max_id=event.message.id,
-                                                   clear_mentions=True,
-                                                   clear_reactions=True,
-                                                   )
-            elif isinstance(event.peer_id, types.PeerChannel):
-                await client.send_read_acknowledge(entity=await client.get_entity(dialog.name),
-                                                   max_id=event.message.id,
-                                                   clear_mentions=True,
-                                                   clear_reactions=True,
-                                                   )
+
+
 
 client.run_until_disconnected()
