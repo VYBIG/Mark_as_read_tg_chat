0e83a19e7c2bb636ca473eb5a76e325b80d126ef
Василий Приказчиков <prikazchikov_v@kazgorset.ru>
2025-02-18 20:50:51 +0300

    lenta_was_added

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ SESSION_DIR = os.getenv('SESSION_DIR')
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 PHONE = os.getenv('PHONE')
+LENTA_ID = os.getenv('LENTA_ID')
+PASSWORD = os.getenv('PASSWORD')
 
--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from telethon.sync import TelegramClient
 from telethon import events, types
-from telethon.tl.functions.messages import ReadHistoryRequest
+from telethon.errors.rpcerrorlist import ChatForwardsRestrictedError
 from config import *
+
 api_id = int(API_ID)
 api_hash = API_HASH
 
 client = TelegramClient(SESSION_DIR, api_id, api_hash,
                         system_version='4.16.30-vxCUSTOM',
                         )
-client.start(phone=PHONE)
+client.start(phone=PHONE, password=PASSWORD)
 
-print('Start to read message from Archive')
+print('Start Telegram Client')
 
+count = 0
+
+
+@client.on(events.NewMessage(incoming=True))
+async def news_to_chat(event):
+    global count
+    if isinstance(event.original_update, types.UpdateNewChannelMessage) and event.message.post:
+        if event.message.grouped_id is None:
+            try:
+                await client.forward_messages(entity=int(LENTA_ID),
+                                              messages=event.message,
+                                              from_peer=event.peer_id)
+            except ChatForwardsRestrictedError:
+                pass
+        else:
+            try:
+                group_id = event.message.grouped_id
+                messages = [mes for mes in await client.get_messages(event.peer_id, limit=10)
+                            if mes.grouped_id == group_id]
+                count += 1
+                if count == len(messages):
+                    messages = await client.get_messages(event.peer_id, limit=count)
+                    await client.forward_messages(entity=int(LENTA_ID),
+                                                  messages=messages,
+                                                  from_peer=event.peer_id)
+                    count = 0
+            except ChatForwardsRestrictedError:
+                pass
 
 
 @client.on(events.NewMessage(incoming=True))
